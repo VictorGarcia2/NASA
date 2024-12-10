42f5ac385fa284c483ad27d711f12c3a3430870a
VictorGarcia2 <victrgarciaphoto@gmail.com>
2024-12-10 12:58:40 -0600

    act

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,13 @@ const date = document.querySelector('#date-picker')
 const card = document.querySelector('.card')
 const peticion = document.querySelector('#peticion')
 peticion.addEventListener('click', () => {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=ANxSr2MF24pdqvlnrfVWfs0xsGxiibQBUqZ3uR0S&date=${date.value}`)
-        .then((response) => response.json())
-        .then((data) => create(data))
+    if(date.value !== ""){
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=ANxSr2MF24pdqvlnrfVWfs0xsGxiibQBUqZ3uR0S&date=${date.value}`)
+            .then((response) => response.json())
+            .then((data) => create(data))
+    } else{
+        date.classList.add('border-2', 'border-red-700', 'bg-red-700/20', 'text-white')
+    }
 })
 function create(valor) {
     console.log(valor);
